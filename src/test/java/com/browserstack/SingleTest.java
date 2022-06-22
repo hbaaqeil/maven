3027482dbc3aa14ce2e208aeb2896545fec30d70
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-23 00:48:11 +0100

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -10,13 +10,14 @@ public class SingleTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
-        driver.get("https://ask.com");
-        Thread.sleep(5000);
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("BrowserStack");
+        driver.get("http://34.246.169.246:8080/");
+        WebElement element = driver.findElement(By.name("j_username"));
+        element.sendKeys("hbaaqeil");
+        WebElement element = driver.findElement(By.name("j_password"));
+        element.sendKeys("Hba@@--01");
         element.submit();
         Thread.sleep(5000);
 
-        Assert.assertEquals("browserstack, Ask.com", driver.getTitle());
+        Assert.assertEquals("Dashboard [Jenkins]", driver.getTitle());
     }
 }
