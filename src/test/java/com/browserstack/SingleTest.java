70b1bb76fb65ff6353f95b6c53ba1315cc439cc3
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-22 23:00:21 +0100

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -11,7 +11,7 @@ public class SingleTest extends BrowserStackTestNGTest {
     @Test
     public void test() throws Exception {
         driver.get("https://www.google.com/ncr");
-        driver.findElement(By.id("L2AGLb")).click()
+        driver.findElement(By.id("L2AGLb")).click();
         Thread.sleep(1000);
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("BrowserStack");
