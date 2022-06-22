3d185eee0201a5a0b5d9ffec2919668997560f16
hbaaqeil <66650114+hbaaqeil@users.noreply.github.com>
2022-06-23 00:22:58 +0100

    Update SingleTest.java

--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -10,12 +10,12 @@ public class SingleTest extends BrowserStackTestNGTest {
 
     @Test
     public void test() throws Exception {
-        driver.get("https://www.google.com/ncr");
+        driver.get("https://ask.com");
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("BrowserStack");
         element.submit();
         Thread.sleep(5000);
 
-        Assert.assertEquals("BrowserStack - Google Search", driver.getTitle());
+        Assert.assertEquals("browserstack, Ask.com", driver.getTitle());
     }
 }
